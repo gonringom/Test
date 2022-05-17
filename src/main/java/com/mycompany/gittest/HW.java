5f1df274bbc7ea2db705ba602bf31f42c1646bf7
gonringom <gringom936@g.educaand.es>
2022-05-17 21:21:09 +0200

    INICIO

---
+++ src/main/java/com/mycompany/gittest/HW.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.gittest;
+
+
+public class HW {
+
+    
+    public static void main(String[] args) {
+        System.out.println("AND IT WILL COME LIKE A FLOOD OF PAIN");
+    }
+    
+}
